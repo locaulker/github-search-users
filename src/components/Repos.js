f6c086507c4b781f2d3d1348ccbbf1943a504319
Lawrence <locaulker@gmail.com>
2022-08-01 06:56:35 -0400

    resume Fusion Charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -7,7 +7,19 @@ const Repos = () => {
   const { repos } = React.useContext(GithubContext)
   console.log(repos)
 
-  return <h2>repos component</h2>
+  const chartData = [
+    { label: "HTML", value: "13" },
+    { label: "CSS", value: "160" },
+    { label: "JavaScript", value: "80" }
+  ]
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  )
 }
 
 const Wrapper = styled.div`
